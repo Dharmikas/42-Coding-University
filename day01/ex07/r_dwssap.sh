d99d1d740a93973de6b4ff75f259279fb7e098e1
Dharmik Shah <dharmikashit.shah@sjsu.edu>
2018-04-02 18:05:07 -0700

    Update r_dwssap.sh

--- day01/ex07/r_dwssap.sh
+++ day01/ex07/r_dwssap.sh
@@ -1,5 +1,2 @@
-#0!/bin/sh
-FT_LINE1=8
-FT_LINE2=16
-
+#!/bin/sh
 cat /etc/passwd | grep -v "^#" | sed -n 'n;p' | cut -d : -f 1 | rev | sort -r | head -n $FT_LINE1 | tail -n $FT_LINE2 | tr '\n' ',' | sed 's/,/, /g' | sed 's/\(.*\). /\1./' | tr -d "\n"
