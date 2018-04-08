6ec0e16c834554490fe62ba6bb9f15a8ae1c3c85
Dharmik Shah <dshah@e1z4r8p18.42.us.org>
2018-04-08 16:06:43 -0700

    day05 ex18

---
+++ day05/ex18/ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/20 10:10:18 by dshah             #+#    #+#             */
+/*   Updated: 2018/03/20 23:06:55 by dshah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int len;
+	unsigned int src_len;
+
+	i = 0;
+	len = 0;
+	while (dst[len] && len < size)
+		len++;
+	i = len;
+	while (src[len - i] && len + 1 < size)
+	{
+		dst[len] = src[len - 1];
+		len++;
+	}
+	if (i < size)
+		dst[size] = '\0';
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	return (i + src_len);
+}
