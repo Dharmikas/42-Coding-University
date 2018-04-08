bcda4dd13b09c6240170514b83a94403910fbe9c
Dharmik Shah <dharmikashit.shah@sjsu.edu>
2018-04-08 16:22:46 -0700

    Add files via upload

---
+++ day05/ex19/ft_strlcpy.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/20 11:57:53 by dshah             #+#    #+#             */
+/*   Updated: 2018/03/20 23:24:32 by dshah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i += 1;
+	return (i);
+}
+
+char	*ft_strncat(char *dest, char *src, int nb)
+{
+	int i;
+	int size;
+
+	i = 0;
+	size = 0;
+	while (dest[size])
+		size++;
+	while (src[i] != '\0' && i < nb)
+	{
+		dest[size] = src[i];
+		i++;
+		size++;
+	}
+	dest[size] = '\0';
+	return (dest);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	ft_strncpy(dst, src, size);
+	return (ft_strlen(src));
+}
